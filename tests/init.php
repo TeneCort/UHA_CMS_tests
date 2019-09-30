fee61864cc3b980c88c2b19674338e18dd35716b
ioannis.karapostolis <ioannis.karapostolis@uha.fr>
2019-09-30 10:30:21 +0200

    cmonnnn

--- tests/init.php
+++ tests/init.php
@@ -1,9 +1,20 @@
 <?php
+define('ROOT'      , dirname(__DIR__)   . DIRECTORY_SEPARATOR);
+define('APP'       , ROOT . 'app' . DIRECTORY_SEPARATOR);
+define('VIEW'      , ROOT . 'app' . DIRECTORY_SEPARATOR . 'view'       . DIRECTORY_SEPARATOR);
+define('MODEL'     , ROOT . 'app' . DIRECTORY_SEPARATOR . 'model'      . DIRECTORY_SEPARATOR);
+define('DATA'      , ROOT . 'app' . DIRECTORY_SEPARATOR . 'data'       . DIRECTORY_SEPARATOR);
+define('CORE'      , ROOT . 'app' . DIRECTORY_SEPARATOR . 'core'       . DIRECTORY_SEPARATOR);
+define('CONTROLLER', ROOT . 'app' . DIRECTORY_SEPARATOR . 'controller' . DIRECTORY_SEPARATOR);
+define('OBJECTS'   , ROOT . 'app' . DIRECTORY_SEPARATOR . 'objects'    . DIRECTORY_SEPARATOR);
 
+$modules = [ROOT,APP,CORE,CONTROLLER,DATA,OBJECTS];
 
-define('OBJECTS', '../app/objects/');
-
-foreach (glob(OBJECTS . "*.php") as $filename)
-{
-	require_once($filename);
+foreach (glob(OBJECTS . "*.php") as $filename) {
+	require $filename;
 }
+/*highlight_string("<?php\n\$data =\n" . var_export($modules, true) . ";\n?>");*/
+set_include_path(ROOT . PATH_SEPARATOR . implode(PATH_SEPARATOR, $modules));
+spl_autoload_register('spl_autoload', false);
+
+?>
