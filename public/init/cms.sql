f860c35ffd0f5cbf7825ef345b40cb8d290348fa
ioannis.karapostolis <ioannis.karapostolis@uha.fr>
2019-10-07 16:54:56 +0200

    testing db

--- public/init/cms.sql
+++ public/init/cms.sql
@@ -16,11 +16,16 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `article`
+-- Current Database: `cms`
 --
 
-USE cms;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `cms` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
 
+USE `cms`;
+
+--
+-- Table structure for table `article`
+--
 
 DROP TABLE IF EXISTS `article`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -33,7 +38,7 @@ CREATE TABLE `article` (
   `page` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `category` (`category`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -42,7 +47,7 @@ CREATE TABLE `article` (
 
 LOCK TABLES `article` WRITE;
 /*!40000 ALTER TABLE `article` DISABLE KEYS */;
-INSERT INTO `article` VALUES (1,'hello','Contenu',1,1),(2,'Titre de ma demo','<p><strong>Contntu de ma demo</strong></p><ul><li>dfsdfsdf</li><li>sdfsdfsdf</li><li>sdfsdf</li></ul>',2,2),(3,'New','helohelohelo',2,1);
+INSERT INTO `article` VALUES (1,'hello','world',1,1);
 /*!40000 ALTER TABLE `article` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -66,7 +71,7 @@ CREATE TABLE `category` (
 
 LOCK TABLES `category` WRITE;
 /*!40000 ALTER TABLE `category` DISABLE KEYS */;
-INSERT INTO `category` VALUES (1,'cate'),(2,'Dev');
+INSERT INTO `category` VALUES (1,'category');
 /*!40000 ALTER TABLE `category` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -139,7 +144,164 @@ CREATE TABLE `page` (
 
 LOCK TABLES `page` WRITE;
 /*!40000 ALTER TABLE `page` DISABLE KEYS */;
-INSERT INTO `page` VALUES (1,'page1'),(2,'UHA_4POINT0');
+INSERT INTO `page` VALUES (1,'page');
+/*!40000 ALTER TABLE `page` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `webPage`
+--
+
+DROP TABLE IF EXISTS `webPage`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `webPage` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `WebPageName` tinytext COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `webPage`
+--
+
+LOCK TABLES `webPage` WRITE;
+/*!40000 ALTER TABLE `webPage` DISABLE KEYS */;
+INSERT INTO `webPage` VALUES (1,'Index');
+/*!40000 ALTER TABLE `webPage` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Current Database: `cms`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `cms` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
+
+USE `cms`;
+
+--
+-- Table structure for table `article`
+--
+
+DROP TABLE IF EXISTS `article`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `article` (
+  `id` smallint(6) NOT NULL AUTO_INCREMENT,
+  `title` tinytext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `textContent` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `category` int(11) DEFAULT NULL,
+  `page` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `category` (`category`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `article`
+--
+
+LOCK TABLES `article` WRITE;
+/*!40000 ALTER TABLE `article` DISABLE KEYS */;
+INSERT INTO `article` VALUES (1,'hello','world',1,1);
+/*!40000 ALTER TABLE `article` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `category` (
+  `id` tinyint(4) NOT NULL AUTO_INCREMENT,
+  `name` tinytext COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `category`
+--
+
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` VALUES (1,'category');
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `navBar`
+--
+
+DROP TABLE IF EXISTS `navBar`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `navBar` (
+  `id` smallint(6) NOT NULL AUTO_INCREMENT,
+  `color` smallint(6) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `color` (`color`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `navBar`
+--
+
+LOCK TABLES `navBar` WRITE;
+/*!40000 ALTER TABLE `navBar` DISABLE KEYS */;
+INSERT INTO `navBar` VALUES (1,4);
+/*!40000 ALTER TABLE `navBar` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `navbar_color`
+--
+
+DROP TABLE IF EXISTS `navbar_color`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `navbar_color` (
+  `id` tinyint(4) NOT NULL AUTO_INCREMENT,
+  `color` tinytext COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `navbar_color`
+--
+
+LOCK TABLES `navbar_color` WRITE;
+/*!40000 ALTER TABLE `navbar_color` DISABLE KEYS */;
+INSERT INTO `navbar_color` VALUES (1,'bg-primary'),(2,'bg-secondary'),(3,'bg-success'),(4,'bg-danger'),(5,'bg-warning'),(6,'bg-info'),(7,'bg-light'),(8,'bg-dark'),(9,'bg-white'),(10,'bg-transparent');
+/*!40000 ALTER TABLE `navbar_color` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `page`
+--
+
+DROP TABLE IF EXISTS `page`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `page` (
+  `id` smallint(6) NOT NULL AUTO_INCREMENT,
+  `name` tinytext COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `page`
+--
+
+LOCK TABLES `page` WRITE;
+/*!40000 ALTER TABLE `page` DISABLE KEYS */;
+INSERT INTO `page` VALUES (1,'page');
 /*!40000 ALTER TABLE `page` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -176,4 +338,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-09-24 16:37:11
+-- Dump completed on 2019-10-07 15:55:15
---
+++ tests/DbConnectionTests.php
@@ -0,0 +1,110 @@
+<?php
+
+require_once('init.php');
+
+use PHPUnit\Framework\TestCase;
+
+ class DbConnectionTests extends TestCase
+ {
+
+	static protected $host, $user, $pass, $name, $conn;
+
+	private $article;
+
+	protected function setUp() : void
+	{
+
+        $this::$host = '0.0.0.0';
+        $this::$user = 'root';
+        $this::$pass = 'example';
+        $this::$name = 'cms';
+
+		try 
+		{
+	        $this::$conn = new PDO(
+	            'mysql:host=' . $this::$host . ';port=3307;dbname=' . $this::$name, $this::$user, $this::$pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
+	        	);
+        }
+        catch (PDOException $e) 
+        {
+            die('Erreur : ' . $e->getMessage());
+        }
+
+        $req = ("SELECT 
+                    a.id          AS a_id, 
+                    a.title       AS a_title, 
+                    a.textContent AS a_textContent, 
+                    a.category    AS a_category, 
+                    a.page        AS a_page,
+                    c.name        AS c_name,
+                    c.id          AS c_id,
+                    p.name        AS p_name,
+                    p.id          AS p_id
+
+                FROM 
+                    article a,
+                    category c,
+                    page p
+                WHERE
+                    c.id = a.category
+                AND
+                    p.id = a.page    
+                ");
+
+        $res = $this::$conn->query($req);
+
+        while ($row = $res->fetch(PDO::FETCH_OBJ)) { 
+        	$this->article = $row;
+        }     
+	}
+
+    public function testConnection()
+	{
+		$this->assertIsObject($this::$conn);
+	}
+
+	public function testReadArticleID()
+	{
+        $this->assertSame('1', $this->article->a_id); 
+    }
+
+	public function testReadArticleTitle()
+	{
+        $this->assertSame('hello', $this->article->a_title); 
+    }
+
+	public function testReadArticleTextContent()
+	{
+        $this->assertSame('world', $this->article->a_textContent); 
+    }
+
+    public function testReadArticleCategoryID()
+	{
+        $this->assertSame('1', $this->article->a_category); 
+    }
+
+    public function testReadArticlePageID()
+	{
+        $this->assertSame('1', $this->article->a_page); 
+    }
+
+    public function testReadCategoryName()
+	{
+        $this->assertSame('category', $this->article->c_name); 
+    }
+
+    public function testReadCategoryID()
+	{
+        $this->assertSame('1', $this->article->a_id); 
+    }
+
+    public function testReadPageName()
+	{
+        $this->assertSame('page', $this->article->p_name); 
+    }
+
+    public function testReadPageID()
+	{
+        $this->assertSame('1', $this->article->p_id);
+    }
+}
