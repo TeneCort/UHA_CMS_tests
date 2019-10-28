7d01e4903a2ec50f66d18bbf6efb721f8715cd27
ioannis.karapostolis <ioannis.karapostolis@uha.fr>
2019-10-28 12:37:54 +0100

    Will the routes work?

---
+++ tests/RoutesTest.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once('init.php');
+require_once('curltest.php');
+
+use PHPUnit\Framework\TestCase;
+
+ class RoutesTests extends TestCase
+ {
+
+    private $routeResponse;
+
+    protected function setUp() : void
+    {
+
+    }
+
+    public function testHomeIndexRoute()
+    {
+        //var_dump(phpinfo(32));
+        //echo("http://" . $_SERVER['REMOTE_ADDR'] . ":" . $_SERVER["SERVER_PORT"] . "/");
+        $routeResponse = CurlRoutes("localhost:8000/home/index");;
+        $this->assertEquals(preg_match('/200/', $routeResponse), '1');
+    }
+    public function testAdminRoute()
+    {
+        $routeResponse = CurlRoutes("localhost:8000/administrationn");;
+        $this->assertEquals(preg_match('/404/', $routeResponse), '1');
+    }
+}
---
+++ tests/curltest.php
@@ -0,0 +1,33 @@
+<?php
+
+function CurlRoutes($url)
+{
+	//$host = "http://" . $_SERVER['REMOTE_ADDR'] . ":" . $_SERVER["SERVER_PORT"] . "/";
+
+	//$url = $host . $url;
+
+	$ch = curl_init();
+
+	  curl_setopt($ch, CURLOPT_URL, $url);
+	  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	  curl_setopt($ch, CURLOPT_HEADER, 1);
+
+	  $response = curl_exec($ch);
+	  
+	  // Return headers seperatly from the Response Body
+	  $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+	  $headers = substr($response, 0, $header_size);
+	  $body = substr($response, $header_size);
+	  
+	curl_close($ch);
+
+	$headers = explode("\r\n", $headers); // The seperator used in the Response Header is CRLF (Aka. \r\n) 
+
+	$headers = array_filter($headers);
+
+	// Isolates the header response code
+	$html = $headers[0];
+
+	return $html;
+}
+?>
